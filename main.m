7e18c1c7ea57b6884dbae11eea7d7d336ddbbcda
ma581 <ma581@cam.ac.uk>
2015-11-12 21:01:56 +0000

    Updating main outline

--- main.m
+++ main.m
@@ -10,30 +10,31 @@
 % ant.PheremoneGain = 0.3;
 % ant.ObstacleAvoidanceGain = 0.1;
 % ant.RandomMotionGain = 0.1;
+% ant.Rho = 10;       %light intensity to rotational speed constant
 
 
 
 %MAZE STRUCTURE
 % maze.Walls = n by m matrix (boolean)
-% maze.EdgeList = x by 4 matrix of connected points
 % maze.Goal = exit/food position
+% maze.PotentialField = n by m matrix
 
 %PHEREMONE STRUCTURE
 % pher.DecayRate
-% pher.Matrix = n by m matrix with pheremone strengths
+% pher.PotentialField = n by m matrix with pheremone strengths
 
 
 %Generate maze (JW's function)
 
 
-%Calculate maze.EdgeList (John's function) 
+%Generate maze potential field(John's function) 
 
 
 %Optimization and solving:
 
 %Repeat over eg 100 iterations{
     
-    % Read pheremone matrix
+    % Read and combine maze.PotentialField and pher.PotentialField
     % Initialize eg 10 ants
     
         %Iterate over time steps till eg 1000 time steps{
